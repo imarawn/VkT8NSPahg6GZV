2eb8ec07e08db1fc5ddd93fe6eff3a4366ce0449
imarawn <142156357+imarawn@users.noreply.github.com>
2024-10-04 11:07:05 +0200

    Create tip recieved.js

---
+++ tip recieved.js
@@ -0,0 +1,4 @@
+if ($room.owner === 'hannasthesia' || $app.version === 'Testbed'){
+addTokies($user.username, $tip.tokens)
+if (gamerunning() && getUserList()[$user.username] && getUserList()[$user.username].participant && $tip.tokens === letterTip)handleGuess(handleTipnote($tip.message))
+}
